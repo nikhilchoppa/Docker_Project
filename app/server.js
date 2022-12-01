de2d3229889d118b863b0f7cec22437431d87150
nikhilsaireddychoppa <nchoppa@uvm.edu>
2022-12-01 15:27:49 -0500

    docker-compose.yaml and DockerFile added

--- app/server.js
+++ app/server.js
@@ -38,7 +38,7 @@ app.post('/update-profile', function (req, res) {
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
-    let db = client.db(databaseName);
+    let db = client.db('user-account');
     userObj['userid'] = 1;
 
     let myquery = { userid: 1 };
@@ -60,7 +60,7 @@ app.get('/get-profile', function (req, res) {
   MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
     if (err) throw err;
 
-    let db = client.db(databaseName);
+    let db = client.db("user-account");
 
     let myquery = { userid: 1 };
 
